1f805b5d142c80360a07fe5a78ecdc9349fb39ca
scott306lr <scott306lr@gmail.com>
2024-11-26 14:24:22 +0800

    feat: :memo: Update requirements and run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 # read opts for CUDA_VISIBLE_DEVICES, LOGLEVEL, and other options
-CUDA_VISIBLE_DEVICES=6
+CUDA_VISIBLE_DEVICES=0
 LOGLEVEL=DEBUG
 while getopts ":d:l:" opt; do
   case $opt in
@@ -12,16 +12,30 @@ while getopts ":d:l:" opt; do
   esac
 done
 
+# Basic options
 LLM_PATH=meta-llama/Llama-2-7b-chat-hf
-SSM_PATH=~/checkpoints/shrinkeagle/pretrain/l1ce/model_10
-# SSM_PATH=~/checkpoints/shrinkeagle/linear/new/l1ce/model_20
-# SSM_PATH=~/checkpoints/shrinkeagle/linear/new/ignore-last/pretrain/l1ce/model_20
-# SSM_PATH=~/checkpoints/eagle/d1/l1ce/fp32/model_20
+SSM_PATH=~/checkpoints/eagle
+SEED=9991
+DO_WARMUP=True
+MAX_NEW_TOKENS=500
+DO_SAMPLE=False
+TEMPERATURE=0
 
-MODE=sd-shrink-eagle
-# MODE=sd-eagle
+# mode options
+MODE=naive #sd-eagle
+
+# sd specific options
 SD_METHOD=greedy
 LAYERS=1
-SEED=9991
 
-LOGLEVEL=$LOGLEVEL CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python run_test.py -nw --max-new-tokens 180 --temp 1.0 --do-sample --mode $MODE --sd-method $SD_METHOD -llm $LLM_PATH -ssm $SSM_PATH --layers $LAYERS --seed $SEED
+# args for run_test.py
+args="--mode $MODE --sd-method $SD_METHOD -llm $LLM_PATH -ssm $SSM_PATH --layers $LAYERS --seed $SEED --max-new-tokens $MAX_NEW_TOKENS"
+if [ $DO_WARMUP = False ]; then
+  args="$args -nw"
+fi
+if [ $DO_SAMPLE = True ]; then
+  args="$args --do-sample --temp $TEMPERATURE"
+fi
+
+# execute run_test.py
+LOGLEVEL=$LOGLEVEL CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python run_test.py $args
