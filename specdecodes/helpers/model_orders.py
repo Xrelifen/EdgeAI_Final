69a483d6842a4d5a056ec39492dfbad5cd24d1ee
scott306lr <scott306lr@gmail.com>
2025-03-07 00:19:30 +0800

    fix(model_orders.py): :bug: Correct llama layer order.

--- specdecodes/helpers/model_orders.py
+++ specdecodes/helpers/model_orders.py
@@ -3,21 +3,20 @@
 def get_llama_layer_order(model_config):
     layers = []
     layers.append('model.embed_tokens')
+    layers.append('model.rotary_emb')
     for i in range(model_config.num_hidden_layers):
         layers.append(f'model.layers.{i}.input_layernorm')
         layers.append(f'model.layers.{i}.self_attn.q_proj')
         layers.append(f'model.layers.{i}.self_attn.k_proj')
         layers.append(f'model.layers.{i}.self_attn.v_proj')
         layers.append(f'model.layers.{i}.self_attn.o_proj')
+        layers.append(f'model.layers.{i}.post_attention_layernorm')
         layers.append(f'model.layers.{i}.mlp.gate_proj')
         layers.append(f'model.layers.{i}.mlp.up_proj')
         layers.append(f'model.layers.{i}.mlp.down_proj')
-        layers.append(f'model.layers.{i}.post_attention_layernorm')
     layers.append('model.norm')
     layers.append('lm_head')
     return layers
-        
-
 
 
 MODEL_TYPE_GET_LAYER_ORDER = {
