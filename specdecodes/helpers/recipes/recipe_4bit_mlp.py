69d60fb2de311a47baf8ffbfa2829518688989c0
scott306lr <scott306lr@gmail.com>
2025-03-07 10:03:02 +0800

    refactor(recipe_4bit_mlp): :recycle: Move estimate_quantized_size function to utils.py and simplify VRAM estimation in recipe function.

--- specdecodes/helpers/recipes/recipe_4bit_mlp.py
+++ specdecodes/helpers/recipes/recipe_4bit_mlp.py
@@ -1,25 +1,6 @@
 from hqq.core.quantize import *
+from ..utils import estimate_quantized_size
 
-
-def estimate_quantized_size(model, quant_config, max_input_len=0):
-    weight_bytes = 0
-    for name, param in model.named_parameters():
-        layer_name = ".".join(name.split(".")[:-1])
-        if layer_name in quant_config:
-            nbits = quant_config[layer_name]['weight_quant_params']['nbits']
-            group_size = quant_config[layer_name]['weight_quant_params']['group_size']
-            weight_bytes += param.numel() * param.element_size() * nbits / 16 # quantized weight
-            weight_bytes += param.numel() * param.element_size() / group_size * 2 # scale and zero
-        else:
-            weight_bytes += param.numel() * param.element_size()
-    
-    for name, param in model.named_buffers():
-        weight_bytes += param.numel() * param.element_size()
-        
-    element_size = next(iter(model.parameters())).element_size() # assume activation has same element size as first param
-    head_size = model.config.hidden_size // model.config.num_attention_heads
-    activation_bytes = 2 * max_input_len * model.config.num_hidden_layers * model.config.num_key_value_heads * head_size * element_size # key and value cache
-    return weight_bytes + activation_bytes
 
 def recipe(model, draft_model, max_length, vram_limit, dtype, device):
     # Quantization
@@ -38,9 +19,8 @@ def recipe(model, draft_model, max_length, vram_limit, dtype, device):
         quant_config[f"model.layers.{i}.mlp.up_proj"] = base_quant_config
         quant_config[f"model.layers.{i}.mlp.down_proj"] = base_quant_config
         
-    max_input_len = 1024
-    estimate_qmodel_size = estimate_quantized_size(model, quant_config, max_length)
-    print(f"Estimated required VRAM for {max_input_len} input tokens: {estimate_qmodel_size / 1024**3:.2f} GiB")
+    estimate_qmodel_size = estimate_quantized_size(model, quant_config)
+    print(f"Model required VRAM: {estimate_qmodel_size / 1024**3:.2f} GiB")
         
     # Device map
     device_map = {}
