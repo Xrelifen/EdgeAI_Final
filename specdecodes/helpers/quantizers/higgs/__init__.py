47c7c51dd847653edb3ea0e16740fca14d10b8ab
scott306lr <scott306lr@gmail.com>
2025-03-14 04:26:32 +0800

    fix(higgs): Update logging message to clarify quantization speed

--- specdecodes/helpers/quantizers/higgs/__init__.py
+++ specdecodes/helpers/quantizers/higgs/__init__.py
@@ -4,5 +4,5 @@ from .hf.base import AutoHiggsHFModel
 class HiggsQuantizer:
     @classmethod
     def quantize_model(cls, model, quant_config, compute_dtype, device):
-        logging.info("Quantizing model with HiggsQuantizer. First few iterations may be very slow.")
+        logging.info("Quantizing model with HiggsQuantizer. First few iterations may be much slower.")
         AutoHiggsHFModel.quantize_model(model, quant_config, compute_dtype=compute_dtype, device=device)
